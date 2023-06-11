b536d980f8c6680e05679e96ec1329871a3c79ac
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-06-11 18:38:24 +0300

    Fix when sentence is empty first letter should be None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence is None or len(sentence) == 0:
         return len(sentence), None
     for letter in sentence:
         if letter == sentence[0]:
