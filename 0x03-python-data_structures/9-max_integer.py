b24a600877986f543d8e3eb17104e2840e58234d
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-06-11 18:59:52 +0300

    Fix error from when max is a negative number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return None
-    max_int = 0
+    max_int = my_list[0]
     for x in my_list:
         if x > max_int:
             max_int = x
