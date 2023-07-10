e8b658520cbfdc360f3ef61554ffa0f15471d094
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-07-11 02:36:43 +0300

    Add string representation

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" module that defines a square """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines a class Square that inherits from Rectangle
+    """
+    def __init__(self, size):
+        """
+        Initialize using a constructor
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns a string representation
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
