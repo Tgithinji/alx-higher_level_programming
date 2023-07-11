039115532908cf64074b2e44f108535c3861b13c
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-07-11 17:35:47 +0300

    Function that returns an object representaed by JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines from_json_string function
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns a Python data structure
+    """
+    return json.loads(my_str) 
