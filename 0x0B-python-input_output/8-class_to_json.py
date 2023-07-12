28cc24848385afec2d2f8fa674b9bcf0c56d0060
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-07-13 00:59:37 +0300

    Fix an exception where the function should not import a module

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,11 @@
 """
 This module defines class_to_json function
 """
-import json
 
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data structures
+    Takes an object as input and
+    Returns its dictionary description with simple data structures
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
