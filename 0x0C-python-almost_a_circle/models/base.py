6741a4c51670c36e1ef5e5681545842697f3f006
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-07-15 18:16:46 +0300

    Write a class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is rhe base module
+"""
+
+
+class Base():
+    """
+    This creates instances of class Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initilizes instances of Base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id =  Base.__nb_objects
