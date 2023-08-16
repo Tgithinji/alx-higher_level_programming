5847281647236eeafcb9b79916125665c9b9d63f
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-08-16 19:39:46 +0300

    Script that lists all records of a table in order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all record of the table second_table
+-- Results should display both the score and name
+-- Score should be ordered
+SELECT score, name FROM second_table ORDER BY score DESC;
