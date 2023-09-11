31eca67627e1390283c5565aeca97f46c2eee9c7
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-11 16:17:46 +0300

    Js script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const firstLanguage = 'C is fun\n';
+const secondLang = 'Python is cool\n';
+const thirdLang = 'JavaScript is amazing';
+
+console.log(firstLanguage + secondLang + thirdLang);
