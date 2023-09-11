4f748f0b6aba8a103799b2e302bb7e3d857c7efd
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-11 16:44:56 +0300

    script that checks for command arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// first to elements are node and name of script
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+	console.log('No argument');
+} else if (argv.length === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Argument found');
+}
