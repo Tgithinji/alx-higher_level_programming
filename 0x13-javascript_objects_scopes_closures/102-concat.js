693d1cc5117eb71b96ae7584e73bebf83c1e1681
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-14 03:09:02 +0300

    Code fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,18 +5,17 @@ const argv = process.argv.slice(2);
 let data = '';
 fs.readFile(argv[0], (err, fileData) => {
   if (err) {
-    console.log(err);
+    return;
   }
   data = fileData.toString();
 
   fs.readFile(argv[1], (err, fileData2) => {
     if (err) {
-      console.log(err);
+      return;
     }
     data += fileData2.toString();
 
-    fs.writeFile(argv[2], data, (err) => {
-      console.log(err);
+    fs.writeFile(argv[2], data, () => {
     });
   });
 });
