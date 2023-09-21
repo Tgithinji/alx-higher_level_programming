a53ff33acef68ae36a714719c70724358898e5f1
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-21 19:30:15 +0300

    first asqlalchemy model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains a class definition of a stste and an instance Base
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    This class inherits from Base
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
