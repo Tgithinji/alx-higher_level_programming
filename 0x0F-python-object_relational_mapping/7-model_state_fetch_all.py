9dad1e608fc693d3d9dd4d82b4112a1938494129
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-21 23:53:04 +0300

    Script that lists all state objects

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module lists all State objects from a database
+"""
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    url = f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
+    engine = create_engine(url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(f"{instance.id}:{instance.name}")
