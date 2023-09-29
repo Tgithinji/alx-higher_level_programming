b42aca60b795c29d16e6492eabe6df2cc75ee221
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-09-29 19:23:39 +0300

    Fetch a url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# This script fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+
+        print("Body response:")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode('utf-8')}")
+
